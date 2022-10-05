4549fe4253cdcee68c9a37c40e17b28bedac16b5
TrellixVulnTeam <kasimir.schulz@trellix.com>
2022-10-05 09:48:33 +0000

    Adding tarfile member sanitization to extractall()

--- kaldi_tflite/lib/models/kaldi/download.py
+++ kaldi_tflite/lib/models/kaldi/download.py
@@ -97,4 +97,23 @@ def downloadModel(link: str, outPath: str, sha256: str = None):
     # Unzipping tarball.
     with tarfile.open(tarPath) as tarFile:
         contents = [tarinfo for tarinfo in tarFile.getmembers()]
-        tarFile.extractall(os.path.dirname(outPath), members=contents)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tarFile, os.path.dirname(outPath), members=contents)
